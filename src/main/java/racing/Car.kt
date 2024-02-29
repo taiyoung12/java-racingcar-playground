6b58330421b19885fef5998face1201e63652266
taiyoung <qq3222@naver.com>
2024-02-29 18:58:30 +0900

    자동차는 스스로 Move를 판단할 수 있다

--- src/main/java/racing/Car.kt
+++ src/main/java/racing/Car.kt
@@ -2,7 +2,8 @@ package racing
 
 class Car(
     private val name: String,
-    private var position: Int = 0
+    private var position: Int = 0,
+    private val randomNumber: RandomNumber
 ) {
     init {
         require(name.length <=5) {"자동차 이름은 5글자 이하 여야만 한다."}
@@ -14,6 +15,15 @@ class Car(
 
         return position
     }
+    fun moveSelf(): Int {
+        if(4 < randomNumber.generateNumber(10)){
+            this.position +=1
+        }
+
+        return position
+    }
+
     fun getName(): String = this.name
     fun getPosition(): Int = this.position
+
 }
--- src/test/java/racing/RacingTest.kt
+++ src/test/java/racing/RacingTest.kt
@@ -6,21 +6,23 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
-import org.mockito.Mockito.doReturn
-import org.mockito.Mockito.lenient
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
 class RacingTest {
     private val anyCarName: String = "R8"
 
-    private val car: Car = Car(anyCarName)
+    private lateinit var car: Car
 
     @Mock
     lateinit var randomNumber: RandomNumber
 
     @BeforeEach
     fun setUp(){
+        MockitoAnnotations.openMocks(this)
+        car = Car(anyCarName, 0, randomNumber)
         lenient().doReturn(5).`when`(randomNumber).generateNumber(10)
         lenient().doReturn(13).`when`(randomNumber).generateNumber(20)
     }
@@ -28,7 +30,7 @@ class RacingTest {
     @Test
     fun `자동차 이름은 5글자 이하 이다`() {
         val exception = assertThrows(IllegalArgumentException::class.java) {
-            Car("AudiR8")
+            Car("AudiR8", 0, randomNumber)
         }
 
         assertTrue(car.getName().length <= 5, "자동차 이름은 5글자 이하이어야 한다.")
@@ -56,7 +58,7 @@ class RacingTest {
 
     @Test
     fun `4 이상의 난수가 나왔을 때 자동차는 움직일 수 있다`(){
-        val actual = Car(anyCarName, 0)
+        val actual = Car(anyCarName, 0, randomNumber)
 
         actual.move(5)
         actual.move(6)
@@ -67,11 +69,33 @@ class RacingTest {
 
     @Test
     fun `4 이하의 난수가 나왔을 때 자동차는 움직일 수 없다`(){
-        val actual = Car(anyCarName, 0)
+        val actual = Car(anyCarName, 0, randomNumber)
 
         actual.move(3)
 
         assertThat(actual.getPosition()).isEqualTo(0)
     }
+
+
+    @Test
+    fun `자동차가 난수를 스스로 뽑을 수 있다`(){
+        `when`(randomNumber.generateNumber(10)).thenReturn(5)
+        val actual = Car(anyCarName, 0, randomNumber)
+
+        actual.moveSelf()
+
+        assertThat(actual.getPosition()).isEqualTo(1)
+    }
+
+    @Test
+    fun `자동차가 난수를 스스로 뽑을 수 있고 5 미만이면 전진할 수 없다`(){
+        `when`(randomNumber.generateNumber(10)).thenReturn(4)
+        val actual = Car(anyCarName, 0, randomNumber)
+
+        actual.moveSelf()
+
+        assertThat(actual.getPosition()).isEqualTo(0)
+    }
+
 
 }
